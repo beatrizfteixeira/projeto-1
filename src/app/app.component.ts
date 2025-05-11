0a30b566d81ca14a27eba924bac51d11c4c0ed81
beatrizfteixeira <beatriz.teixeira@estudante.ufla.br>
2025-05-11 00:22:47 -0300

    primeiro-componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HelloWorldAppComponent } from './hello-world-app/hello-world-app.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorldAppComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/hello-world-app/hello-world-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component(
+    {
+        selector: 'hello-world',
+        template: '<h1>oeee mundon</h1>'
+    }
+)
+export class HelloWorldAppComponent {
+
+}
